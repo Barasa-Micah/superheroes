4229ab9dad41ea702473c1cdacbfb5ac3e5c2399
Barasa-Micah <barasamica@gmail.com>
2023-10-01 13:24:51 +0300

    Created the database

--- superheroes/app.py
+++ superheroes/app.py
@@ -14,6 +14,8 @@ db.init_app(app)
 
 
 if __name__ == '__main__':
-    db.create_all()
+    # Creating the database tables if they don't exits that is
+    with app.app_context():
+     db.create_all()
 
     app.run(debug=True)
--- superheroes/models.py
+++ superheroes/models.py
@@ -4,16 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Hero(db.model):
+class Hero(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(255), nullable= False)
 
-class Power(db.model):
+class Power(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     description=db.Column(db.String(255), nullable=False)
 
-class HeroPower(db.model):
+class HeroPower(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     hero_id=db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
-    power_id=db.Column(db.Integer, db.ForeignKey('power.id', nullable=False))
+    power_id=db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
     strength=db.Column(db.Integer, nullable=False)
